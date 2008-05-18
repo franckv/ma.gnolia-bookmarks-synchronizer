f4fe8fc461ad4f7051351d9ba3e83de6645a41e2
franck <franck.vdl@gmail.com>
2008-05-18 20:52:51 +0200

    getting more than 500 bookmarks is now possible

--- chrome/content/status.js
+++ chrome/content/status.js
@@ -1,3 +1,40 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ *   Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is magnolia-sync.
+ *
+ * The Initial Developer of the Original Code is
+ * Franck.
+ * Portions created by the Initial Developer are Copyright (C) 2008
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ * 
+ * ***** END LICENSE BLOCK ***** */
+
+
 var set_status = function(val) {
     var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
     var enumerator = wm.getEnumerator('navigator:browser'), win;
@@ -23,15 +60,21 @@ var insert_bookmark = function(uri, title) {
 
 var update_bookmarks = function() {
     var req = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance();
-    var bmsvc = Components.classes["@mozilla.org/browser/nav-bookmarks-service;1"].getService(Components.interfaces.nsINavBookmarksService);
-    var ios = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
 
-    var url = 'http://ma.gnolia.com/api/rest/1/bookmarks_find?person=myself&limit=10&api_key=' + get_api_key();
+    var todate = null;
 
     document.getElementById('magnolia-sync-panel').setAttribute('src', 'chrome://magnolia-sync/content/loader.gif');
     //set_status('Updating bookmarks...');
-    req.open('GET', url, true);
-    req.onreadystatechange = function (aEvt) {
+    var findurl = 'http://ma.gnolia.com/api/rest/1/bookmarks_find?person=myself&api_key=' + get_api_key();
+    req.open('GET', findurl, true);
+    req.onreadystatechange = function() {handleRequest(req);};
+    req.send(null); 
+};
+
+var handleRequest = function(req) {
+    var bmsvc = Components.classes["@mozilla.org/browser/nav-bookmarks-service;1"].getService(Components.interfaces.nsINavBookmarksService);
+    var ios = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
+    try {
 	if (req.readyState == 4) {
 	    if (req.status == 200) {
 		var doc = req.responseXML;
@@ -42,6 +85,7 @@ var update_bookmarks = function() {
 		    var title = nodes[i].getElementsByTagName('title')[0].textContent;
 		    var url  = nodes[i].getElementsByTagName('url')[0].textContent;
 		    var created = nodes[i].getAttribute('created');
+		    todate = created;
 		    var updated = nodes[i].getAttribute('updated');
 		    var uri = ios.newURI(url, null, null);
 		    if (!bmsvc.isBookmarked(uri)) {
@@ -53,23 +97,35 @@ var update_bookmarks = function() {
 		    else {
 			++bmupdated;
 		    }
-		   
+
 		    var tags = nodes[i].getElementsByTagName('tag');
 		    tag_uri(uri, tags);
-		    
+
 		    var description  = nodes[i].getElementsByTagName('description')[0].textContent;
 		    annotate_uri(uri, description);
 		} 
+		if (nodes.length == 500 && todate != null) {
+		    var findurl = 'http://ma.gnolia.com/api/rest/1/bookmarks_find?person=myself&api_key=' + get_api_key();
+		    req.open('GET', findurl + '&to=' + todate);
+		    req.onreadystatechange = function() {handleRequest(req);};
+		    req.send(null); 
+		}
+		else {
+		    document.getElementById('magnolia-sync-panel').setAttribute('src', 'chrome://magnolia-sync/content/icon.png');
+		}
 		//set_status(nodes.length);
-		document.getElementById('magnolia-sync-panel').setAttribute('src', 'chrome://magnolia-sync/content/icon.png');
-		alert(bmadded + ' new bookmarks, ' + bmupdated + ' updated');
 	    }
-	    else
-		alert("Error loading page\n");
+	    else {
+		alert("Error loading page: " + req.status);
+		document.getElementById('magnolia-sync-panel').setAttribute('src', 'chrome://magnolia-sync/content/icon.png');
+	    }
 	}
-    };
-    req.send(null); 
+    } catch(e) {
+	alert("Error loading page: " + e);
+	document.getElementById('magnolia-sync-panel').setAttribute('src', 'chrome://magnolia-sync/content/icon.png');
+    }
 };
+
 
 var date_string_to_number = function(datestr) {
     datestr = datestr.replace(/-/, '/');
